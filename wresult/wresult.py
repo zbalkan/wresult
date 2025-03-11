5b97a07fef289eae04aa982047b2e2ba207fc532
Zafer Balkan <zafer@zaferbalkan.com>
2025-03-11 17:09:27 +0200

    Fixed multiple agent_config blocks when os, name or profile is set

--- wresult/wresult.py
+++ wresult/wresult.py
@@ -23,20 +23,62 @@ class EnhancedJSONEncoder(json.JSONEncoder):
 class Conf:
     content: dict
 
+
+@dataclass
+class FinalConf():
+
+    def __init__(self, ossec_conf: Conf, agent_conf: Conf) -> None:
+        # self.content = ossec_conf.content.copy()
+        # self.content.update(agent_conf.content)
+        # TODO: consolidate the two configurations
+
+        c = ossec_conf.content.copy().get("ossec_config", {})
+        a = agent_conf.content.copy().get("agent_config", {})
+
+        for key, value in a.items():
+            if c.get(key) is None:
+                c[key] = value
+            else:
+                if isinstance(c[key], list):
+                    if isinstance(value, list):
+                        c[key].extend(value)
+                    else:
+                        c[key].append(value)
+                elif isinstance(c[key], dict):
+                    # Get to the child objects
+                    c[key].update(value)
+                else:
+                    c[key] = [c[key], value]
+
+        self.content = c
+
     def to_json(self, indent: Optional[int] = None) -> str:
         return json.dumps(self.content, cls=EnhancedJSONEncoder, indent=indent)
 
 
-@dataclass
-class FinalConf(Conf):
+class ConfParser:
 
-    def __init__(self, ossec_conf: Conf, agent_conf: Conf) -> None:
-        self.content = ossec_conf.content.copy()
-        self.content.update(agent_conf.content)
-        # TODO: consolidate the two configurations
+    agent_os: str
+    agent_name: str
+    agent_profile: list[str]
 
+    def __init__(self) -> None:
+        # get OS info
+        if os.name == 'posix':
+            self.agent_os = "Linux"
+        else:
+            self.agent_os = "Windows"
 
-class Parser:
+        # Get agent name and profile
+        if os.name == 'posix':
+            agent_info_path = '/var/ossec/etc/.agent_info'
+        else:
+            agent_info_path = 'C:/Program Files (x86)/ossec-agent/.agent_info'
+
+        with open(agent_info_path, "r", encoding="utf-8") as file:
+            lines = file.readlines()
+            self.agent_name = lines[0].strip()
+            self.agent_profile = lines[3].replace(' ', '').replace(r'\n', '').split(",")
 
     def parse_conf(self, file_path: Union[pathlib.Path, str]) -> Conf:
         with open(file_path, "r", encoding="utf-8") as file:
@@ -47,12 +89,12 @@ class Parser:
         content: dict = xmltodict.parse(
             '<root>' + text + '</root>').get("root", {})
 
-        self.__consolidate_config_items(content)
+        self.__deduplicate_blocks(content)
 
         content = OrderedDict(sorted(content.items()))
         return Conf(content=content)
 
-    def __consolidate_config_items(self, content) -> None:
+    def __deduplicate_blocks(self, content) -> None:
         root = list(content.items())[0]
         # root[1] is either ossec_config or agent_config
         if isinstance(root[1], list):
@@ -60,13 +102,28 @@ class Parser:
             for i, internal_dict in enumerate(root[1]):
                 if i == 0:
                     continue
-                for key, value in internal_dict.items():
-                    if new_content.get(key) is None:
-                        new_content[key] = value
-                    else:
-                        if not isinstance(new_content[key], list):
-                            new_content[key] = [new_content[key]]
-                        new_content[key].append(value)
+                # Handle config per os, profile or name
+                # https://documentation.wazuh.com/current/user-manual/reference/centralized-configuration.html#options
+                if internal_dict.get('@os') is not None:
+                    if re.compile(internal_dict.get('@os')).match(self.agent_os):
+                        new_content.update(internal_dict)
+                elif internal_dict.get('@profile') is not None:
+                    if re.compile(internal_dict.get('@profile')).match(self.agent_profile):
+                        new_content.update(internal_dict)
+                elif internal_dict.get('@name') is not None:
+                    if re.compile(internal_dict.get('@name')).match(self.agent_name):
+                        new_content.update(internal_dict)
+                else:
+                    for key, value in internal_dict.items():
+                        if new_content.get(key) is None:
+                            new_content[key] = value
+                        else:
+                            if not isinstance(new_content[key], list):
+                                new_content[key] = [new_content[key]]
+                            if isinstance(value, list):
+                                new_content[key].extend(value)
+                            else:
+                                new_content[key].append(value)
 
             content[root[0]] = new_content
 
@@ -114,7 +171,7 @@ def main() -> None:
     else:
         ossec_conf_path = str(args.ossec_conf_path)
 
-    policy_parser = Parser()
+    policy_parser = ConfParser()
     agent_conf = policy_parser.parse_conf(agent_conf_path)
     ossec_conf = policy_parser.parse_conf(ossec_conf_path)
     final = FinalConf(ossec_conf, agent_conf)
